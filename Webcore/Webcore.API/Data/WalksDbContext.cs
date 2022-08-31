511f4aa33e31c42813e6b6c8ef2f578ac5db6a49
ImanHassoon <iman.h.1439@gmail.com>
2022-09-01 00:30:22 +0300

    Section 2_Iman

---
+++ Webcore/Webcore.API/Data/WalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Webcore.API.Models.Domain;
+
+namespace Webcore.API.Data
+{
+    public class WalksDbContext: DbContext
+    {
+        public WalksDbContext(DbContextOptions<WalksDbContext> options): base(options)
+        {
+
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+
+    }
+}
