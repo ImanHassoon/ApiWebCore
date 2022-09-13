e12b3457b5aa28cf35c1a7c1af103adb1498febf
ImanHassoon <39035092+ImanHassoon@users.noreply.github.com>
2022-09-13 13:34:05 +0300

    section 6

--- Webcore/Webcore.API/Controllers/WalkDifficultiesController.cs
+++ Webcore/Webcore.API/Controllers/WalkDifficultiesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Webcore.API.Repositories;
 
 namespace Webcore.API.Controllers
@@ -8,16 +9,65 @@ namespace Webcore.API.Controllers
     public class WalkDifficultiesController : Controller
     {
         private readonly IWalkDifficultyRepository walkDifficultyRepository;
+        private readonly IMapper mapper;
 
-        public WalkDifficultiesController(Repositories.IWalkDifficultyRepository walkDifficultyRepository)
+        public WalkDifficultiesController(Repositories.IWalkDifficultyRepository walkDifficultyRepository, IMapper mapper)
         {
             this.walkDifficultyRepository = walkDifficultyRepository;
+            this.mapper = mapper;
         }
         [HttpGet]
         public async Task<IActionResult> GetAllWalkDifficulties()
         {
-           return Ok(await walkDifficultyRepository.GetAllAsync());
+            var walkdiff = await walkDifficultyRepository.GetAllAsync();
+            var walkdiffDto=mapper.Map<List<Models.DTO.WalkDifficulty>>(walkdiff);
+           return Ok(walkdiffDto);
          
+        }
+        
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetWalkDifficultybyId")]
+        public async Task<IActionResult> GetWalkDifficultybyId(Guid id)
+        {
+            var walkdiff=await walkDifficultyRepository.GetAsync(id);
+            if (walkdiff == null)
+                return NotFound();
+            // convert domain object to DTO
+            var walkdiffDto=mapper.Map<Models.DTO.WalkDifficulty>(walkdiff);
+            return Ok(walkdiffDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalkDifficultyASync(Models.DTO.AddWalkDifficultyRequest walkDifficultyRequest)
+        {
+            var walkDiffDomain = new Models.Domain.WalkDifficulty
+            {
+                Code = walkDifficultyRequest.Code,
+            };
+            walkDiffDomain = await walkDifficultyRepository.AddAsync(walkDiffDomain);
+
+       
+
+            var walkDiffDto=mapper.Map<Models.DTO.WalkDifficulty>(walkDiffDomain);
+
+            return CreatedAtAction(nameof(GetWalkDifficultybyId),new {id=walkDiffDto.Id},walkDiffDto);
+
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public  async Task<IActionResult> UpdateWalkDiffAsync(Guid id, Models.DTO.UpdateWalkDifficultyRequest updateWalkDifficultyRequest)
+        {
+            var walkDiffDomain = new Models.Domain.WalkDifficulty
+            {
+                Code = updateWalkDifficultyRequest.Code,
+            };
+            if (walkDiffDomain==null)
+                return NotFound();
+            var walkDiffDto = mapper.Map<Models.DTO.WalkDifficulty>(walkDiffDomain);
+            return Ok(walkDiffDto);
+
         }
     }
 }
--- Webcore/Webcore.API/Repositories/WalkDifficultyRepository.cs
+++ Webcore/Webcore.API/Repositories/WalkDifficultyRepository.cs
@@ -12,10 +12,38 @@ namespace Webcore.API.Repositories
         {
             this.walksDbContext = walksDbContext;
         }
+
+        public async Task<WalkDifficulty> AddAsync(WalkDifficulty walkDifficulty)
+        {
+            walkDifficulty.Id=Guid.NewGuid();
+            await walksDbContext.WalkDifficulties.AddAsync(walkDifficulty);
+            await walksDbContext.SaveChangesAsync();
+            return walkDifficulty;
+        }
+
         public async Task<IEnumerable<WalkDifficulty>> GetAllAsync()
         {
             return await walksDbContext.WalkDifficulties.ToListAsync();
            
+        }
+
+        public async Task<WalkDifficulty> GetAsync(Guid id)
+        {
+            return await walksDbContext.WalkDifficulties.FirstAsync(x => x.Id == id);
+            
+        }
+
+        public async Task<WalkDifficulty> UpdateAsync(Guid id,WalkDifficulty walkDifficulty)
+        {
+          var existWalkdiff= await walksDbContext.WalkDifficulties.FindAsync(id);
+            if (existWalkdiff != null)
+            {
+                existWalkdiff.Code = walkDifficulty.Code;
+                await walksDbContext.SaveChangesAsync();
+                return existWalkdiff;
+            }
+            return null;
+            
         }
     }
 }
