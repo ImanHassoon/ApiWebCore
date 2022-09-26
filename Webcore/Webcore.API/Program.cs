4c1357e9a3b8cdde636675805362f9df9c163dad
ImanHassoon <39035092+ImanHassoon@users.noreply.github.com>
2022-09-26 08:16:45 +0300

    test

--- Webcore/Webcore.API/Program.cs
+++ Webcore/Webcore.API/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer(); // test
 builder.Services.AddSwaggerGen(options =>
 {
     var securityScheme = new OpenApiSecurityScheme
